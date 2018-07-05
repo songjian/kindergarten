54f7e80b87d03c8bb35c301e57c409761fef6dd3
songjian <oldsong@codeorder.cn>
2018-07-05 11:04:29 +0800

    调整adminlte静态资源文件

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -12,23 +12,14 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require jquery
+//= require jquery/dist/jquery
 //= require jquery_ujs
 //= require jquery-ui/ui/widget
 //= require bootstrap/dist/js/bootstrap
-//= require raphael/raphael
-//= require morris.js/morris
+//= require fastclick/lib/fastclick
+//= require admin-lte/dist/js/adminlte
 //= require jquery-sparkline/jquery.sparkline
 //= require admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min
 //= require admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en
-//= require jquery-knob/dist/jquery.knob.min
-//= require moment/min/moment.min
-//= require bootstrap-daterangepicker/daterangepicker
-//= require bootstrap-datepicker/dist/js/bootstrap-datepicker
-//= require admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all
 //= require jquery-slimscroll/jquery.slimscroll.js
-//= require fastclick/lib/fastclick
-//= require admin-lte/dist/js/adminlte
-//= require admin-lte/dist/js/pages/dashboard
-//= require admin-lte/dist/js/demo
 //= require kindeditor
