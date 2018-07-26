4cecf53e3c4533f0aed6105d26a2fa3ecf770a70
songjian <oldsong@codeorder.cn>
2018-07-26 21:42:52 +0800

    经减掉公告管理、食物管理和食物分类管理。

--- db/migrate/20180711081055_create_food_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateFoodCategories < ActiveRecord::Migration[5.1]
-  def change
-    create_table :food_categories, comment: '食物分类表' do |t|
-      t.string :name, comment: '食物分类名'
-
-      t.timestamps
-    end
-  end
-end
